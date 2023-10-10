2adf2fe3f1d6925ee3b9cb6b66c2f506b3429fb4
mhuzaifi0604 <huzzaifaasim@gmail.com>
2023-10-11 00:39:06 +0500

    All Gather & All Gather V commented and completed

--- AllGather.c
+++ AllGather.c
@@ -1,34 +1,51 @@
+/*
+Muhammad Huzaifa
+20i-0604
+Assignment # 02
+Task # 01 Part 01 === AllGather using MPI_send & MPI_Recv
+*/
+
+// Inclusing necessory libraries
 #include <mpi.h>
 #include <stdio.h>
 #include<stdlib.h>
-#include<time.h>
 
 int main(int argc, char **argv){
+    // declaring variable one time outside any clustered env
     int rank, size, name_len;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
+    
+    // Initializing MPI Sequence 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Retreiving rank of process/ container b/w parallel processing
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Retreiving size of process/ container b/w parallel processing
+    
+    //initializing data to send with the rank of the process
     int data_to_send = rank;
-    int *data_to_recv = (int*)malloc(size*sizeof(int));
+    int *data_to_recv = (int*)malloc(size*sizeof(int)); // creating a dynamic buffer to recv data
+
+    // Running loop for all processes/ containers => sending data to all other slaves
     for(int i = 0 ; i < size; i++){
-        if(i != rank){
+        if(i != rank){// Sending data if rank is not processes own rank 
+            // Sending data to another process (rank == i) -> for each container
             MPI_Send(&data_to_send, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            // Recieving data from all other processes in recv buffer
             MPI_Recv(&data_to_recv[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }else{
-            data_to_recv[i] = data_to_send;
+            data_to_recv[i] = data_to_send; // data to recv is same if rank is processes' own
         }
     }
+    // getting container id to show code is working on containers
     MPI_Get_processor_name(processor_name, &name_len);
     // Printing data recieved to each process
     printf("\n\t\t \x1B[35m==========\x1B[0m Process with Rank # \x1B[33m%d\x1B[0m, Container ID # \x1B[31m%s\x1B[0m \x1B[35m========== \x1B[0m\n", rank, processor_name);
     printf("Data recieved: [");
     for(int i = 0 ; i < size; i++){
-        printf("\x1B[32m%d\x1B[0m ", data_to_recv[i]);
+        printf("\x1B[32m%d\x1B[0m ", data_to_recv[i]); // printing data recieved by each process
     }
     printf("]\n");
-    free(data_to_recv);
-    MPI_Finalize();
+    free(data_to_recv); // deallocating the memory allocated to recieve buffer
+    MPI_Finalize(); // finallizing the MPI sequence
     return 0;
 
 }
--- AllGatherv.c
+++ AllGatherv.c
@@ -1,34 +1,99 @@
-#include <mpi.h>
-#include <stdio.h>
-#include<stdlib.h>
-#include<time.h>
+/*
+Muhammad Huzaifa
+20i-0604
+Assignment # 02
+Task # 01 Part 02 === AllGatherv using MPI_send & MPI_Recv
+*/
 
-int main(int argc, char **argv){
+// Including Necessory Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+    // Initializing MPI sequence
+    MPI_Init(&argc, &argv);
+    // Declaring variables for later use for each container
     int rank, size, name_len;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int data_to_send = rank;
-    int *data_to_recv = (int*)malloc(size*sizeof(int));
-    for(int i = 0 ; i < size; i++){
-        if(i != rank){
-            MPI_Send(&data_to_send, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Recv(&data_to_recv[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }else{
-            data_to_recv[i] = data_to_send;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Retreiving rank of process/ container b/w parallel processing
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Retreiving size of process/ container b/w parallel processing
+
+    // DDefining variable sixe of data to be sent to each slave
+    int variable_data_size = rank + 1;
+    int* data_to_send = (int*)malloc(variable_data_size * sizeof(int)); // initializing memory fior data to send using size calculated
+
+    // Prinitng the data sent by each process of variable size
+    printf("\n\n \x1B[33mPrinting my data array woth size\x1B[0m: \x1B[31m%d\x1B[0m\n\n", variable_data_size);
+    for (int i = 0; i < variable_data_size; i++) {
+        data_to_send[i] = (rank + 1) * 10 + i; // adding data to send data variable (each time of diff size) since each time data size = rank + 1 
+        printf("\x1B[32m%d\x1B[0m ", data_to_send[i]); // printing the elements of data to be sent 
+    }
+    printf("\nDone printing Data of length %d", variable_data_size);
+
+    // Declaring vars and arrays to keep track of elements sent by each process, places to store data in recv buffer and total data sent by the slaves
+    int total_data_recved = 0;
+    int* var_counts = (int*)malloc(size * sizeof(int));
+    int* places = (int*)malloc(size * sizeof(int));
+
+    // Determine the total number of elements to be received by each process
+    for (int i = 0; i < size; i++) {
+        var_counts[i] = i + 1; // calculating total data to be recved in accordance to rank + 1
+        places[i] = total_data_recved; // data will be placed at places[i] in buffer [i] for each rank
+        total_data_recved += var_counts[i];
+    }
+
+    // Allocate memory for the receive buffer
+    int* recvbuf = (int*)malloc(total_data_recved * sizeof(int));
+
+    // Perform the gather operation
+    for (int i = 0; i < size; i++) {
+        if (i != rank) { // SEnding data if process' rank is not its own\
+            // Sending data of variable size to each other slave + sending the size of dat that is sent each time
+            MPI_Send(&variable_data_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(data_to_send, variable_data_size, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
     }
+
+    // Recieving variable length data from each slave
+    for (int i = 0; i < size; i++) {
+        if (i != rank) { // checking if rank of process is not its own
+            int var_recv_size;
+            // Recieving variable data size from all other slaves 
+            MPI_Recv(&var_recv_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // creating a temp buffer and allocating memory to it to store the varibale size data of length size recieved above
+            int* temp_recvbuf = (int*)malloc(var_recv_size * sizeof(int));
+            MPI_Recv(temp_recvbuf, var_recv_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // loop for placing data at respective index that we calculated above in places 
+            for (int j = 0; j < var_recv_size; j++) {
+                recvbuf[places[i] + j] = temp_recvbuf[j];
+            }
+
+            free(temp_recvbuf); // memory deallocation for temp buffer
+        } else {
+            // Copy the data from data_to_send to the appropriate location in the receive buffer
+            for (int j = 0; j < variable_data_size; j++) {
+                recvbuf[places[i] + j] = data_to_send[j];
+            }
+        }
+    }
+    // getting container id to show code is working on containers
     MPI_Get_processor_name(processor_name, &name_len);
     // Printing data recieved to each process
     printf("\n\t\t \x1B[35m==========\x1B[0m Process with Rank # \x1B[33m%d\x1B[0m, Container ID # \x1B[31m%s\x1B[0m \x1B[35m========== \x1B[0m\n", rank, processor_name);
     printf("Data recieved: [");
-    for(int i = 0 ; i < size; i++){
-        printf("\x1B[32m%d\x1B[0m ", data_to_recv[i]);
+    for (int i = 0; i < total_data_recved; i++) {
+        printf("\x1B[32m%d\x1B[0m ", recvbuf[i]); // printing data recieved by each container / process
     }
     printf("]\n");
-    free(data_to_recv);
-    MPI_Finalize();
-    return 0;
 
+    // Memory deallocation | Clean Up process
+    free(data_to_send);
+    free(recvbuf);
+    free(var_counts);
+    free(places);
+
+    MPI_Finalize(); // Finalizing MPI Parallel Sequence
+    return 0;
 }
